6864f36411edc728390189514a30a409ccb1ede3
LukaszCieplucha <lukasz.cieplucha@beefee.co.uk>
2022-01-09 19:12:54 +0100

    fix get data

--- exchangerates/rates/management/commands/crawler.py
+++ exchangerates/rates/management/commands/crawler.py
@@ -18,14 +18,17 @@ filepath = os.path.join(
 
 
 def get_data():
+    from decimal import Decimal
     for x in cryptos:
         if not soup.find(text=x):
             continue
         else:
             usd_rate = soup.find(text=x).next_element.small.text
-            usd_rate = Decimal(usd_rate.split(' ')[0])
+            usd_rate = usd_rate.replace(' ', '')
+            usd_rate = Decimal(usd_rate[0:len(usd_rate)-3])
             pln_rate = soup.find(text=x).next_element.strong.text
-            pln_rate = Decimal(pln_rate.split(' ')[0])
+            pln_rate = pln_rate.replace(' ', '')
+            pln_rate = Decimal(pln_rate[0:len(pln_rate)-3])
 
             rows.append({'Name': x, 'USD rate': usd_rate, 'PLN rate': pln_rate,
                          'Data + Time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
